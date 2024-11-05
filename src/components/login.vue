cb614e8260234263b2f9d15da0acc0f19f2f0d09
chenbox <a339686214@gmail.com>
2024-11-05 22:09:43 +0800

    update login page

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,7 +1,20 @@
 <template>
-  <form action="login">
-    <label for="userName">UserName:</label>
-  </form>
+  <div class="container">
+    <div class="logo">
+      <img src="/images/icon.jpg" alt="icon-image" width="60px">
+      <router-link to = "/">#CCC</router-link>
+    </div>
+    <form class="inputArea">
+      <label for="userName">UserName:</label>
+      <div class="customInput">
+        <input type="text" name="userName" placeholder="Your Name">
+      </div>
+      <label for="password">Password:</label>
+      <div class="customInput">
+        <input type="password" name="password" placeholder="Your Password">
+      </div>
+    </form>
+  </div>
 </template>
 
 <script setup>
@@ -9,5 +22,36 @@
 </script>
 
 <style scoped>
+.container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: linear-gradient(to bottom, #252525, #505050)
+}
 
+.logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  gap: 20px;
+}
+
+.logo img {
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  background-color: #fff;
+  padding: 10px;
+}
+
+.inputArea {
+  background-color: rgba(160,160, 160, 0.5);
+  padding: 20px;
+  border-radius: 30px;
+}
 </style>
