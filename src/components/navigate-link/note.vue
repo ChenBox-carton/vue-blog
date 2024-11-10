16ab0cd90073aa11692dd6e2780c49eae34bb266
chenbox <a339686214@gmail.com>
2024-11-10 17:22:31 +0800

    note page (1.0)

--- src/components/navigate-link/note.vue
+++ src/components/navigate-link/note.vue
@@ -1,6 +1,10 @@
 <template>
 <navigate/>
-<p>This is Note Page</p>
+<div class="container">
+  <button class="creatNote">
+    <i class='bx bxs-plus-square'></i>
+  </button>
+</div>
 </template>
 
 <script setup>
@@ -8,5 +12,32 @@ import navigate from '../navigate.vue';
 </script>
 
 <style scoped>
+.creatNote {
+  width: 60px;
+  height: 60px;
+  position: absolute;
+  bottom: 30px;
+  right: 30px;
+  background-color: rgb(255, 255, 255);
+  border: none;
+  border-radius: 10px;
+  transition: all 0.4s ease;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+.creatNote i {
+  font-size: 40px;
+  color: #000;
+  transition: all 0.4s ease;
+}
+
+.creatNote:hover {
+  background-color: #ffaf01;
+}
+
+.creatNote i:hover {
+  transform: scale(1.2);
+}
 </style>
